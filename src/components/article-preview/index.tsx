fb2cd3c9c2babbf74f5f759c449ce01c431fd552
tommyfms2 <hidesoop@yahoo.co.jp>
2023-03-19 01:31:44 +0900

    てst

--- src/components/article-preview/index.tsx
+++ src/components/article-preview/index.tsx
@@ -7,9 +7,8 @@ interface Props {
 }
 
 export default function ArticlePrevewCard({ articlePreview }: Props) {
-    const basePath = process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}` : '';
     return (
-        <Link key={articlePreview.id} href={`${basePath}/post/${articlePreview.id}`}>
+        <Link key={articlePreview.id} href={`/post/${articlePreview.id}`}>
             <Col key={articlePreview.id}>
             <Card hoverable style={{ width: 300 }} cover={<img alt={articlePreview.title} src={articlePreview.image_preview} />}>
                 <Card.Meta title={articlePreview.title} description={articlePreview.summary}/>
