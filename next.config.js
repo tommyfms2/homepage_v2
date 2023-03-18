3474d394311675ddeb78c02b017e04f817b01c6a
tommyfms2 <hidesoop@yahoo.co.jp>
2023-03-18 22:28:46 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: './',
+  assetPrefix: process.env.NODE_ENV === 'pr' ? '/homepage_v2/' : '',
+  basePath: process.env.NODE_ENV === 'pr' ? '/homepage_v2/' : '',
 }
 
 module.exports = nextConfig
