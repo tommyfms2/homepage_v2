e7a6252d642a1529db5c134a035c6ab0cfff684a
tommyfms2 <hidesoop@yahoo.co.jp>
2023-03-18 21:31:58 +0900

    てst

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "development" ? "/homepage_v2/" : '',
-  assetPrefix: process.env.NODE_ENV === "development" ? "/homepage_v2/" : '',
+  basePath: process.env.NODE_ENV === "pr-to-develop" ? "/homepage_v2/" : '',
+  assetPrefix: process.env.NODE_ENV === "pr-to-develop" ? "/homepage_v2/" : '',
 }
 
 module.exports = nextConfig
