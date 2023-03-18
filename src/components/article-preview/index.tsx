1676903bf2ae530b3511a999b11308bf082f8659
tommyfms2 <hidesoop@yahoo.co.jp>
2023-03-18 22:41:15 +0900

    test

--- src/components/article-preview/index.tsx
+++ src/components/article-preview/index.tsx
@@ -1,5 +1,7 @@
+import { BASE_PATH } from "@/lib/constants";
 import { Card, Col } from "antd";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { ArticlePreview } from "./types";
 
 interface Props {
@@ -7,8 +9,11 @@ interface Props {
 }
 
 export default function ArticlePrevewCard({ articlePreview }: Props) {
+    const router = useRouter();
+    const basePath = router.pathname.startsWith(BASE_PATH) ? BASE_PATH : '';
+
     return (
-        <Link key={articlePreview.id} href={`/post/${articlePreview.id}`}>
+        <Link key={articlePreview.id} href={`${basePath}/post/${articlePreview.id}`}>
             <Col key={articlePreview.id}>
             <Card hoverable style={{ width: 300 }} cover={<img alt={articlePreview.title} src={articlePreview.image_preview} />}>
                 <Card.Meta title={articlePreview.title} description={articlePreview.summary}/>
---
+++ src/lib/constants.tsx
@@ -0,0 +1,1 @@
+export const BASE_PATH = '/homepage_v2';
